6bce5fc66b57ee17ffcc7da4875fd0b47fda28e8
Harshit Prabtani <harshitprabtani007@gmail.com>
2026-05-01 00:54:02 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,14 +1,9 @@
 <?php
-$host = getenv("DB_HOST");
-$user = getenv("DB_USER");
-$pass = getenv("DB_PASS");
-$db   = getenv("DB_NAME");
-$port = getenv("DB_PORT");
-
-// Optional fallback (in case port empty)
-if (!$port) {
-    $port = 3306;
-}
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$pass = getenv("MYSQLPASSWORD");
+$db   = getenv("MYSQLDATABASE");
+$port = (int)getenv("MYSQLPORT"); // important: convert to int
 
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
