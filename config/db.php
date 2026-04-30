5dfa978cec9c69f06223bba0300bd87fc74129be
Harshit Prabtani <harshitprabtani007@gmail.com>
2026-04-30 23:47:45 +0530

    db.php

--- config/db.php
+++ config/db.php
@@ -1,7 +1,16 @@
 <?php
-$conn = new mysqli("localhost:3308", "root", "", "taskforge");
 
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+$db   = getenv("DB_NAME");
+$port = getenv("DB_PORT");
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
+
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 ?>
