1d797c186625ded23c01a08db4152bd73dd1ce3a
Harshit Prabtani <harshitprabtani007@gmail.com>
2026-05-01 00:45:31 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,16 +1,18 @@
 <?php
-
 $host = getenv("DB_HOST");
 $user = getenv("DB_USER");
 $pass = getenv("DB_PASS");
 $db   = getenv("DB_NAME");
 $port = getenv("DB_PORT");
 
+// Optional fallback (in case port empty)
+if (!$port) {
+    $port = 3306;
+}
+
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
 ?>
